f51e5f678fd5fb402f704f53fa3a1d6c880ff99f
Ankush Kumar <ankushkumae467@gmail.com>
2025-11-28 18:18:02 +0530

    afdsa

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Catch-all
   app.get("*", (req, res) => {
+    
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
