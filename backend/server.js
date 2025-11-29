7048f2d8c7b065468f3a270a308e6bd1e6c2d0e9
Ankush Kumar <ankushkumae467@gmail.com>
2025-11-29 13:36:53 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,10 @@ app.use("/api/message", messageRoutes);
 
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 
 app.listen(PORT, () => {
   ConnectDb();
